fe7a921f60bcc6a0639055faccf87ec2a68cc2d4
Rain Juhl <rainjuhl@gmail.com>
2025-02-28 11:22:38 -0800

    First commit

---
+++ trafficmodel/src/main.cpp
@@ -0,0 +1,32 @@
+#include "config.h"
+
+int main() {
+    std::cout << "In Main" << std::endl;
+
+    GLFWwindow* window;
+
+    if(!glfwInit()){
+        std::cout << "GLFW would not start" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "test_window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Glad loader failed" << std::endl;
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
