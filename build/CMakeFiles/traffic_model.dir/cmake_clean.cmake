414d5a11483b0ba92063bbda2adf57931bd812d3
Rain Juhl <rainjuhl@Rains-MacBook-Pro.local>
2025-02-28 11:22:38 -0800

    First commit

---
+++ build/CMakeFiles/traffic_model.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_model.dir/src/glad.c.o"
+  "CMakeFiles/traffic_model.dir/src/glad.c.o.d"
+  "CMakeFiles/traffic_model.dir/src/main.cpp.o"
+  "CMakeFiles/traffic_model.dir/src/main.cpp.o.d"
+  "traffic_model"
+  "traffic_model.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/traffic_model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ trafficmodel/dependencies/model/vehicle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "config.h"
+
+class Vehicle{
+    private:
+        // Road ptr
+        // Position
+        // Size
+        // Destinations
+        // Directions
+    public:
+        // Move
+        // Init
+}
