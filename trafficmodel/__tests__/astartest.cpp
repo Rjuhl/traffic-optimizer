012e62b6e797930184e0d73eb3004a5e959506e9
Rjuhl <rainjuhl@gmail.com>
2025-03-30 17:17:36 -0700

    wrote astar unit tests

--- trafficmodel/__tests__/astartest.cpp
+++ trafficmodel/__tests__/astartest.cpp
@@ -15,7 +15,7 @@ class SimpleCyclicGraph {
         ###############
         ###0-------0###
         ####\#####/####
-        ######\#/######∏
+        ######\#/######
         #######0#######
         ###############
     */
@@ -73,6 +73,64 @@ class SimpleCyclicGraph {
 
 };
 
+// Simple Test Graph
+class SimpleAcyclicGraph {
+    /*
+        ##################
+        ###0----------0###
+        ####\########/####
+        ######\####/######
+        #######0--0#######
+        ##################
+    */
+
+    private:
+        std::vector<Road*> roads;
+        std::vector<Intersection*> intersections;
+
+    public:
+        SimpleAcyclicGraph() {
+            // Create three intersections
+            for (int i = 0; i < 4; i++) {
+                intersections.push_back(new Intersection(1, 1, false));
+            }
+
+            // Create roads
+            Road* r0 = new Road(Pos(0, 0), Pos(0, 3), intersections[0]);
+            Road* r1 = new Road(Pos(0, 3), Pos(10, 3), intersections[1]);
+            Road* r2 = new Road(Pos(0, 3), Pos(4, 0), intersections[2]);
+            Road* r3 = new Road(Pos(4, 0), Pos(6, 0), intersections[3]);
+            Road* r4 = new Road(Pos(10, 3), Pos(6, 0), intersections[3]);
+            Road* r5 = new Road(Pos(6, 0), Pos(10, 3), intersections[1]);
+
+            //Link roads to intersections
+            intersections[0]->assignIncomingRoad(r0, Direction::BOTTOM);
+            intersections[0]->assignOutgoingRoad(r1, Direction::RIGHT);
+            intersections[0]->assignOutgoingRoad(r2, Direction::BOTTOM);
+            intersections[1]->assignIncomingRoad(r1, Direction::LEFT);
+            intersections[1]->assignIncomingRoad(r5, Direction::BOTTOM);
+            intersections[1]->assignOutgoingRoad(r4, Direction::BOTTOM);
+            intersections[2]->assignOutgoingRoad(r3, Direction::RIGHT);
+            intersections[3]->assignIncomingRoad(r4, Direction::TOP);
+            intersections[3]->assignOutgoingRoad(r5, Direction::TOP);
+
+            //Add roads to roads vecotor
+            roads = {r0, r1, r2, r3, r4, r5};
+        }
+        
+        ~SimpleAcyclicGraph() {
+            for (int i = 0; i < intersections.size(); i++) {
+                delete intersections[i];
+            }
+
+            for (int i = 0; i < roads.size(); i++) {
+                delete roads[i];
+            }
+        }
+
+        std::vector<Road*> getRoads() { return roads; }
+
+};
 
 // Test Single Node
 TEST(AStarSuite, SingleRoad) {
@@ -94,26 +152,90 @@ TEST(AStarSuite, SingleRoad) {
 
 
 // Test invalid end node
-
-// Test invalid start node
-
-// Test simple graph
-TEST(AstarSuite, SimpleGraphTests) {
-    SimpleCyclicGraph graph = SimpleCyclicGraph();
-    std::vector<Road*> roads = graph.getRoads();
+TEST(AstartSuite, InvalidEndNode) {
+    SimpleAcyclicGraph graph = SimpleAcyclicGraph();
+    Intersection* seperateIntersection = new Intersection(1, 1, false);
+    Road* seperateRoad = new Road(Pos(20, 20), Pos(20, 21), seperateIntersection);
     Astar pathFinder = Astar();
 
-    std::cout << "Testing if shortest path is found between to valid roads with a simple network" << std::endl;
-    auto [cost, path] = pathFinder.astar(roads[0], roads[2]);
+    std::cout << "Testing that no path is found with invalid end node" << std::endl;
 
+    // Try to reach an end node not in the graph
+    auto [cost, path] = pathFinder.astar(graph.getRoads()[0], seperateRoad);
     std::cout << "Cost " << cost << std::endl;
     std::cout << "Path size " << path.size() << std::endl;
     helpers.printDirections(path);
 
+    EXPECT_EQ(cost, -1);
+    EXPECT_EQ(path.size(), 0);
 
-    EXPECT_EQ(1, 2);
+    // Try to reach a node that is impossiable to reach in the graph
+    auto [cost2, path2] = pathFinder.astar(graph.getRoads()[2], graph.getRoads()[1]);
+    std::cout << "Cost " << cost2 << std::endl;
+    std::cout << "Path size " << path2.size() << std::endl;
+    helpers.printDirections(path2);
+
+    EXPECT_EQ(cost2, -1);
+    EXPECT_EQ(path2.size(), 0);
+
+    std::cout << std::endl;
+
+    delete seperateRoad;
+    delete seperateIntersection;
 }
 
-// Test medium graph
+// Test invalid start node
+TEST(AstartSuite, InvalidStartNode) {
+    // Test a start node not in the graph
+    SimpleAcyclicGraph graph = SimpleAcyclicGraph();
+    Intersection* seperateIntersection = new Intersection(1, 1, false);
+    Road* seperateRoad = new Road(Pos(20, 20), Pos(20, 21), seperateIntersection);
+    Astar pathFinder = Astar();
 
-// Test large graph
+    auto [cost, path] = pathFinder.astar(seperateRoad, graph.getRoads()[0]);
+    EXPECT_EQ(cost, -1);
+    EXPECT_EQ(path.size(), 0);
+
+    delete seperateRoad;
+    delete seperateIntersection;
+}
+
+// Test simple graph
+TEST(AstarSuite, SimpleGraphTests) {
+    SimpleCyclicGraph cyclicGraph = SimpleCyclicGraph();
+    SimpleAcyclicGraph acyclicGraph = SimpleAcyclicGraph();
+    std::vector<Road*> roads = cyclicGraph.getRoads();
+    std::vector<Road*> roads2 = acyclicGraph.getRoads();
+    Astar pathFinder = Astar();
+
+    std::cout << "Testing if shortest path is found between to valid roads with a simple network" << std::endl;
+    auto [cost, path] = pathFinder.astar(roads[0], roads[2]);
+    std::cout << "Cost " << cost << std::endl;
+    std::cout << "Path size " << path.size() << std::endl;
+    helpers.printDirections(path);
+
+    EXPECT_EQ(11, cost);
+    EXPECT_EQ(std::vector<Direction>({Direction::RIGHT, Direction::LEFT}), path);
+
+    auto [cost2, path2] = pathFinder.astar(roads2[0], roads2[4]);
+    std::cout << "Cost " << cost2 << std::endl;
+    std::cout << "Path size " << path2.size() << std::endl;
+    helpers.printDirections(path2);
+
+    EXPECT_EQ(13, cost2);
+    EXPECT_EQ(std::vector<Direction>({Direction::RIGHT, Direction::BOTTOM}), path2);
+
+    auto [cost3, path3] = pathFinder.astar(roads2[0], roads2[5]);
+    std::cout << "Cost " << cost3 << std::endl;
+    std::cout << "Path size " << path3.size() << std::endl;
+    helpers.printDirections(path3);
+
+    EXPECT_EQ(10, cost3);
+    EXPECT_EQ(std::vector<Direction>({
+        Direction::BOTTOM, Direction::RIGHT, Direction::TOP
+    }), path3);
+
+    std::cout << std::endl;
+}
+
+// Test large graph (Complete once editor/saver/loader are complete)
---
+++ trafficmodel/__tests__/intersectiontest.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+// Test update
+TEST(IntersectionSuite, IntersectionUpdateTests) {
+    // Test that updates switch back and forth
+
+    // Test that countdown is correct
+
+    // Test that countdown is correct when the lights are timed differently
+};
+
+// Test Crossing
+TEST(IntersectionSuite, CrossingTests) {
+    // Test Cross all directions while light is green
+
+    // Test Cross all direction while light is red
+
+    // Test crossing when road is full
+};
